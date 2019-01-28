dfcd3b3dc5b3afccac3824578034f794eb636894
Brian Nguyen <Brian Nguyen@DESKTOP-H9S8RP3>
2019-01-28 08:16:57 -0800

    Update codes

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,5 +1,6 @@
 //
 // Brian T Nguyen
+// Display the sum of multiples numbers of 3 or 5 of a number less then specified limit
 //
 
 
@@ -10,12 +11,12 @@ function isMultiplesOf(num, ofNum) {
         return false;
 }
 
-function sumOfMultiplesOf(num, ofNum) {
-    var i = 0;
+function sumOfMultiplesOf(n1, n2, limit) {
+    var m = Math.min(n1,n2); //determine the smaller numbeer
     var sum = 0;
 
-    for (i = 0; i < num; i++ ) {
-        if (isMultiplesOf(i, ofNum)) {
+    for (var i = m; i < limit; i++ ) {
+        if (isMultiplesOf(i, n1) || isMultiplesOf(i, n2)) {
             sum += i;
         }
     }
@@ -25,17 +26,35 @@ function sumOfMultiplesOf(num, ofNum) {
 
 function sumOfMultiplesNumber()
 {
-    var upperBoundNumber = document.getElementById("inputValue").value;
-    var listOfMultiplesOfNumber = [3,5];
-    var sumOfMultiplesOfNumbers = 0;
+    var limit = document.getElementById("inputValue").value;
+    var sum = 0;
+    var m = Math.min(3,5); //determine the smaller numbeer
+    var message = "If we list all the natural numbers below " + limit + " that are multiples of 3 or 5, we get ";
+ 
+    sum = sumOfMultiplesOf(3, 5, limit);
 
-    for ( i = 0; i < listOfMultiplesOfNumber.length; i++) {
-        sumOfMultiplesOfNumbers += sumOfMultiplesOf(upperBoundNumber, listOfMultiplesOfNumber[i] );
+    document.getElementById("sumOfMultipleOf3And5").innerHTML = "The sum = " +  sum;
+
+    if (sum === 0) {
+        mssage += ' nothing. The sum = 0';
     }
+    else {
+        for (var i = m; i < limit; i++ ) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                if (i === limit -1) {
+                    message = message + i + ".";
+                }
+                else {
+                    message = message + i + ", ";
+                }
+            }
+        }
 
-    document.getElementById("outputValue").innerHTML = "Sum of multiples numbers of 3 or 5 below " + upperBoundNumber + " = " + sumOfMultiplesOfNumbers.toString();
+        message = message + ' The sum = ' + sum.toString();
+    }
+ 
+    document.getElementById("outputValue").innerHTML = message;
     
     // DEBUGGING:
-    //console.log("Sum of multiples numbers of 3 or 5 below " + upperBoundNumber + " = " + sumOfMultiplesOfNumbers);
-
+    //console.log(message);
 }
